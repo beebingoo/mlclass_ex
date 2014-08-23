ef226d28d918ed777ad22b3bfa8e6d9c4237bb05
beebingoo <beebingoo@gmail.com>
2014-08-23 13:50:39 +0800

    Complete ex3.

--- mlclass-ex3-006/mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3-006/mlclass-ex3/lrCostFunction.m
@@ -35,11 +35,12 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
+h = sigmoid(X*theta);
+temp = theta;
+temp(1) = 0;
+J = 1/m*((-y)'*log(h)-(1-y)'*log(1-h)) + lambda/2/m*temp'*temp;
+grad = 1/m*(X'*(h-y));
+grad = grad + lambda/m*temp;
 
 
 
